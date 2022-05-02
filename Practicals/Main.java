4238015ef5d3f51283765664b5508abe52ed1690
PANDIT <87899750+Riteshp2001@users.noreply.github.com>
2022-05-02 17:52:48 +0530

    Update Main.java

--- Practicals/Main.java
+++ Practicals/Main.java
@@ -1,5 +1,10 @@
 public class Main{
   public static void main(String[] args) {
     System.out.println("Hello World");
+    char x1,x2;
+    x1 ='8';
+    x2 ='A';
+    System.out.println("First Number: "+x1);
+    System.out.println("Second Letter: "+x2);
   }
 }
