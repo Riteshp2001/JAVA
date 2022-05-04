e0af2cd305ec085a1f74cf6032c3b0fe2ec90ab0
PANDIT <87899750+Riteshp2001@users.noreply.github.com>
2022-05-04 23:13:40 +0530

    Create Datatypes_tut-1.java

---
+++ HANDS-ON/Datatypes_tut-1.java
@@ -0,0 +1,13 @@
+public class Practice {
+    public static void main(String[] args) {
+        int num_1,num_2;
+        num_1 = 23;
+        num_2 = 32;
+        System.out.println("added number is: "+(num_1+num_2));
+        System.out.println("subtracted number is: "+(num_1-num_2));
+        System.out.println("multiplied number is: "+(num_1*num_2));
+        System.out.println("divided number is: "+(num_1/num_2));
+        System.out.println("modulo number is: "+(num_1%num_2));
+        System.out.println("Power number is: "+Math.pow(num_2,num_1));
+    }
+}
