c5802151ca506d0690344e301ae0a85c0e6c0288
PANDIT <87899750+Riteshp2001@users.noreply.github.com>
2022-05-06 11:53:23 +0530

    Update Datatypes_tut-1.java

--- HANDS-ON/Datatypes_tut-1.java
+++ HANDS-ON/Datatypes_tut-1.java
@@ -1,8 +1,20 @@
+import java.utils.Scanner
+
 public class Practice {
     public static void main(String[] args) {
-        int num_1,num_2;
-        num_1 = 23;
-        num_2 = 32;
+       int num_1,num_2;
+       // num_1 = 23;
+       // num_2 = 32;
+        
+        Scanner sc = new Scanner(System.in); 
+
+        //getting input from user
+        System.out.println("enter first no: ");
+        num_1 = sc.nextInt();
+         System.out.println("Enter seconf Number : ");
+        num_2 = sc.nextInt();
+  
+        //by using Scanner utils in java !
         System.out.println("added number is: "+(num_1+num_2));
         System.out.println("subtracted number is: "+(num_1-num_2));
         System.out.println("multiplied number is: "+(num_1*num_2));
