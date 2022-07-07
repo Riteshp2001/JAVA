15bd2ebdcce5f5c2ccfcacf9d6a85a3d8a42d0d5
PANDIT <87899750+Riteshp2001@users.noreply.github.com>
2022-07-07 18:21:39 +0530

    Update StudentAvgMarks_07_07_2022.java

--- Assignments/Error Handling (07-07-2022)/StudentAvgMarks_07_07_2022.java
+++ Assignments/Error Handling (07-07-2022)/StudentAvgMarks_07_07_2022.java
@@ -1,52 +1,52 @@
 package Lab_Activities;
 
-import java.sql.SQLOutput;
+//Java Program to Display Average Marks of Subjects
 import java.util.Scanner;
 
 interface details{
     void name();
     void marks();
-    void roll_no();
+    void roll_no();//declaring methods
 }
 
 class Marksout implements details{
     Scanner sc = new Scanner(System.in);
     String name;
-    int avg,roll;
+    int avg,roll;//declaring variables
     @Override
     public void name() {
         System.out.print("Enter Your Name: ");
-        name = sc.nextLine();
+        name = sc.nextLine();//taking name input
     }
 
     @Override
     public void roll_no() {
         System.out.print("Enter your rollno: ");
         Scanner sc = new Scanner(System.in);
-        roll = sc.nextInt();
+        roll = sc.nextInt();//taking roll number input
     }
     @Override
     public void marks() {
         Scanner sc = new Scanner(System.in);
         System.out.println("How Many subjects marks you want to Enter: ");
         int count = sc.nextInt();
-        int sum = 0;
-        for(int i= 0;i<count;i++){
+        int sum = 0;//initializing variable
+        for(int i= 0;i<count;i++){//looping through subjects and storing each marks in sum variable
             System.out.println("Enter your "+(i+1)+" subject mark : ");
             int num = sc.nextInt();
             sum = sum+num;
         }
-        avg = sum/5;
-        System.out.println(name+"Your Average Marks of "+count+" subjects is: "+avg);
+        avg = sum/count;//calculating Average
+        System.out.println(name+"Your Average Marks of "+count+" subjects is: "+avg);//displaying average
     }
 
 
 }
 public class StudentAvgMarks_07_07_2022 {
     public static void main(String[] args) {
-        Marksout m = new Marksout();
+        Marksout m = new Marksout();//creating Object of Class MarkOut
         m.name();
         m.roll_no();
-        m.marks();
+        m.marks();//calling functions
     }
 }
