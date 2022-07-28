b0c9f343651b14d73481bf39f0dd364cf46ae349
PANDIT <87899750+Riteshp2001@users.noreply.github.com>
2022-07-28 19:25:07 +0530

    Update Age_verification.java

--- Assignments/Password_validator_28-07-2022(thurs Lab)/Age_verification.java
+++ Assignments/Password_validator_28-07-2022(thurs Lab)/Age_verification.java
@@ -3,8 +3,8 @@ package Login_Inputs;
 
 import java.util.Scanner;
 
-class AgeExp extends Exception {
-    AgeExp(String a) {
+class AgeExp extends Exception {//Creating class withg user defined Exception
+    AgeExp(String a) {//creating parameterized Constructor
         System.out.println(a);
     }
 }
@@ -12,17 +12,17 @@ class AgeExp extends Exception {
 public class Age_verification  {
     Scanner sc = new Scanner(System.in);
     public static void main(String[] args)  {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);//crerating scanner class object
         System.out.print("Enter ur age :");
-        int age = sc.nextInt();
+        int age = sc.nextInt();//user input
 
-        try {
-            if(age < 18)
+        try {//try block
+            if(age < 18)//condition
                 throw new AgeExp("Age is less than 18");
             else
                 System.out.println("Valid age");
         }
-        catch (AgeExp a) {
+        catch (AgeExp a) {//catch block
             System.out.println(a);
         }
     }
